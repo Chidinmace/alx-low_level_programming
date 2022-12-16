9c2d544d00a63d56c30e0ea5265eff3db297baeb
Chidinma Ekwegbalu <111113887+Chidinmace@users.noreply.github.com>
2022-12-16 20:42:28 +0100

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,1 +1,18 @@
-#main
+#include "main.h"
+
+/**
+ * print_numbers - prints number
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
+Footer
+© 2022 GitHub, Inc.
